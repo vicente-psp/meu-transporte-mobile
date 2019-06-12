d3624ddf558101db420a4c067285bbc09955f8d4
matheus621 <matheus621cardoso@gmail.com>
2019-06-12 09:36:55 -0300

    criando exemplo de resource

---
+++ app/src/main/java/com/example/meu_transporte_mobile/resource/UsersResource.java
@@ -0,0 +1,30 @@
+package com.example.meu_transporte_mobile.resource;
+
+
+import com.example.androidretrofit2.model.Users;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+
+
+public interface UsersResource {
+
+    @GET("users")
+    Call<List<Users>> get();
+
+    @POST("users")
+    Call<Users> post(@Body Users user);
+
+    @PUT("users")
+    Call<Users> put(@Body Users user);
+
+    @DELETE("users/{id}")
+    Call<Void> delete(@Path("id") int id);
+}
