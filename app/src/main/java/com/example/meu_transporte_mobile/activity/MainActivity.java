702d0c89c68ba00ecbff9169d8995038898f8efb
matheus621 <matheus621cardoso@gmail.com>
2019-06-12 09:30:08 -0300

    Adicionando os pacotes e configurando o APIClient, do webservice para consumir dados

--- app/src/main/java/com/example/meu_transporte_mobile/activity/MainActivity.java
+++ app/src/main/java/com/example/meu_transporte_mobile/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.meu_transporte_mobile;
+package com.example.meu_transporte_mobile.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.meu_transporte_mobile.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/meu_transporte_mobile/bootstrap/APIClient.java
@@ -0,0 +1,29 @@
+package com.example.meu_transporte_mobile.bootstrap;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+
+public class APIClient {
+
+    private static Retrofit retrofit = null;
+    //DOMINIO+CONTEXTO
+    public static final String ENDPOINT = "https://jsonplaceholder.typicode.com/";
+
+    public static Retrofit getClient() {
+
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl(ENDPOINT)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build();
+
+        return retrofit;
+    }
+}
